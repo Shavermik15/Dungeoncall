12ba8530e6f418aecd94fbf2f65f857eb4393438
Egor <Kot454556@yandex.ru>
2023-07-03 16:18:54 +0300

    НА ДИЗАЙН

--- Dungeoncall/Entity.cs
+++ Dungeoncall/Entity.cs
@@ -6,10 +6,10 @@ using System.Threading.Tasks;
 
 namespace Dungeoncall
 {
-    public abstract class Entity
+    public  class Entity
     {
         private string _name;
-        public string Name { get { return _name; } set { if (value != null) { _name = value; } else { _name = Name; } } }
+        public string Name { get { return _name; } }
         private int hp;
         public int HP
         {
@@ -49,7 +49,54 @@ namespace Dungeoncall
                 }
             }
         }
-        public Entity() { }
+        private int damage;
+        public int Damage
+        {
+            set
+            {
+                if (value < 0)
+                {
+                    damage = 0;
+                }
+            }
+            get { return damage; }
+        }
+        public Entity(string _name, int hp,int armor,int critrate,int damage) 
+        { 
+            this._name = _name;
+            if (hp < 0)
+            {
+                this.hp = 1;
+            }
+            else
+            {
+                this.hp = hp;
+            }
+            if (armor < 0)
+            {
+                this.armor = 0;
+            }
+            else if (armor > 1)
+            {
+                this.armor = 1;
+            }
+            else { this.armor = armor; }
+            if (critrate < 0)
+            {
+                this.critrate = 0;
+            }
+            else if (critrate > 1)
+            {
+                this.critrate = 1;
+            }
+            else { this.critrate = critrate; }
+            if (damage < 0)
+            {
+                this.damage = 0;
+            }
+            else this.damage = damage;
+
+        }
 
 
     }
--- Dungeoncall/Form1.cs
+++ Dungeoncall/Form1.cs
@@ -6,5 +6,11 @@ namespace Dungeoncall
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form2 form2 = new Form2();
+            this.Close();
+        }
     }
 }
