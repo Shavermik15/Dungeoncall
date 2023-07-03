8339a57cfdcc9c8b7f5cb47698f32d3eea2fef43
Egor <Kot454556@yandex.ru>
2023-07-03 16:00:41 +0300

    Aga

--- Dungeoncall/Entity.cs
+++ Dungeoncall/Entity.cs
@@ -6,7 +6,51 @@ using System.Threading.Tasks;
 
 namespace Dungeoncall
 {
-    internal class Entity
+    public abstract class Entity
     {
+        private string _name;
+        public string Name { get { return _name; } set { if (value != null) { _name = value; } else { _name = Name; } } }
+        private int hp;
+        public int HP
+        {
+            set
+            {
+                if (value < 0)
+                {
+                    hp = 0;
+                }
+            }
+            get { return hp; }
+        }
+        private int armor;
+        public int Armor { get { return armor; } set 
+            { 
+                if (value > 0) 
+                    armor = value; 
+                else  armor = 0; 
+                if (value>1)
+                {
+                    armor = 1;
+                }
+            }
+        }
+        private int critrate;
+        public int CritRate
+        {
+            get { return armor; }
+            set
+            {
+                if (value > 0)
+                    armor = value;
+                else armor = 0;
+                if (value > 1)
+                {
+                    armor = 1;
+                }
+            }
+        }
+        public Entity() { }
+
+
     }
 }
